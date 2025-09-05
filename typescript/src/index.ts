d935a349f12865e1915f7910356e295411d40098
BigBoa19 <choinoah2005@gmail.com>
2025-09-05 11:08:14 -0400

    All classes implement shape interface, for java and typescript

--- typescript/src/index.ts
+++ typescript/src/index.ts
@@ -1,6 +1,10 @@
 import { newRenderer } from "./renderer.js"
-import { Rectangle, newRectangle } from "./shapes/rectangle.js";
-
-const rectangle: Rectangle = newRectangle(2, 3)
+import { newRectangle } from "./shapes/rectangle.js";
+import { circle } from "./shapes/circle.js";
+const rectangle: Shape = newRectangle(2, 3)
 const renderer = newRenderer(rectangle)
 renderer.draw();
+
+const circleShape: Shape = circle(5);
+const circleRenderer = newRenderer(circleShape);
+circleRenderer.draw();
